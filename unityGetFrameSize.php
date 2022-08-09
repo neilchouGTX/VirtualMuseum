20f080633a3f8867f000061b7108195d8c612293
neilchouGTX <neil12386@hotmail.com.tw>
2022-08-09 22:35:58 +0800

    add unity GetFrameSize

---
+++ unityGetFrameSize.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("connMysql.php");
+    if(isset($_GET["museum_name"]) && isset($_GET["art_place"]) && isset($_GET["school_id"])){
+        $stmt = $conn->prepare("SELECT `image_id` FROM `".$_GET["school_id"]."_exhibition_hall` WHERE exhibition_name=? AND art_place=?");
+        $stmt -> bind_param("ss",$_GET["museum_name"],$_GET["art_place"]);    
+        $stmt -> execute();
+        $stmt -> bind_result($fetch_image_id);
+        $stmt -> fetch();
+        $stmt -> close();
+        if(isset($fetch_image_id)){
+            $stmt = $conn->prepare("SELECT `image_path` FROM `".$_GET["school_id"]."_image_attribute` WHERE image_id=?");
+            $stmt -> bind_param("s",$fetch_image_id);
+            $stmt -> execute();
+            $stmt -> bind_result($fetch_image_path);
+            $stmt -> fetch();
+            $stmt -> close();
+            list($width, $height, $type, $attr) = getimagesize($fetch_image_path);
+            echo $width/$height;
+        }
+    }    
+?>
